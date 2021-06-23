02625845ed0e3d1e0ff5faaa2a6208b1ebb0e67b
jaykabu <jaykabutarwala2@gmail.com>
2021-06-23 14:18:31 +0530

    relational data practies

--- src/practical.js
+++ src/practical.js
@@ -1,26 +1,167 @@
+// import {GraphQLServer} from "graphql-yoga";
+//
+// // Scalar type => String, Boolean, Int, Float, ID
+//
+// // Type Definitions (Schema)
+// const typeDefs = `
+//     type Query {
+//         title: String!
+//         price: Float!
+//         releaseYear: Int!
+//         rating: Float!
+//         inStock: Boolean!
+//     }
+// `
+//
+// // Resolvers
+// const resolvers = {
+//     Query: {
+//         title: () => 'chocolate',
+//         price: () => 12.99,
+//         releaseYear: () => 1992,
+//         rating: () => 4.3,
+//         inStock: () => true
+//     }
+// }
+//
+// const server = new GraphQLServer({typeDefs, resolvers});
+//
+// server.start(() => {
+//     console.log("The server is up!")
+// })
+
 import {GraphQLServer} from "graphql-yoga";
 
-// Scalar type => String, Boolean, Int, Float, ID
+//demo user data
+const users = [
+    {
+        id: '1',
+        name: 'John',
+        email: 'john.doe@gmail.com',
+        age: 25
+    },
+    {
+        id: '2',
+        name: 'Doe',
+        email: 'Doe@gmail.com'
+    },
+    {
+        id: '3',
+        name: 'Jay',
+        email: 'jay@gmail.com',
+        age: 23
+    }
+];
+
+const posts = [
+    {
+        id: '001',
+        title: 'JavaScript',
+        body: 'create javaScript project',
+        published: true,
+        author: '3'
+    },
+    {
+        id: '002',
+        title: 'React JS',
+        body: 'create reactJs project',
+        published: true,
+        author: '3'
+    },
+    {
+        id: '003',
+        title: 'NodeJs',
+        body: 'create NodeJs project',
+        published: false,
+        author: '1'
+    },
+]
 
 // Type Definitions (Schema)
 const typeDefs = `
     type Query {
+        posts(query: String): [Post!]!
+        users(query: String): [User!]!
+        me: User!
+        post: Post!
+    }
+    
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+        posts: [Post!]!
+    }
+    
+    type Post{
+        id: ID!
         title: String!
-        price: Float!
-        releaseYear: Int!
-        rating: Float!
-        inStock: Boolean!
-    }    
+        body: String!
+        published: Boolean!
+        author: User!
+    }
 `
 
 // Resolvers
 const resolvers = {
     Query: {
-        title: () => 'chocolate',
-        price: () => 12.99,
-        releaseYear: () => 1992,
-        rating: () => 4.3,
-        inStock: () => true
+        posts: (parent, args, ctx, info) => {
+            if (!args.query) {
+                return posts
+            }
+
+            return posts.filter((post) => {
+                return post.title.toLowerCase().includes(args.query.toLowerCase());
+            })
+        },
+
+        users: (parent, args, ctx, info) => {
+            // console.log(args.query)
+            if (!args.query) {
+                return users
+            }
+            return users.filter((user) => {
+                const isTitleMatch = user.name.toLowerCase().includes(args.query.toLowerCase());
+                const isBodyMatch = user.body.toLowerCase().includes(args.query.toLowerCase());
+
+                return isTitleMatch || isBodyMatch;
+            })
+        },
+
+        me: () => {
+            return {
+                id: '123455',
+                name: 'Jay',
+                email: 'jay@gmail.com',
+                age: 23
+            }
+        },
+
+        post: () => {
+            return {
+                id: '087',
+                title: 'Graph Ql',
+                body: 'create api',
+                published: true
+            }
+        }
+    },
+
+    Post: {
+        author: (parent, args, ctx, info) => {
+            return users.find((user) => {
+                return user.id === parent.author;
+            })
+        }
+    },
+
+    User: {
+        posts: (parent, args, ctx, info) => {
+            return posts.filter(post => {
+                return post.author === parent.id
+            })
+        }
     }
 }
 
@@ -28,4 +169,7 @@ const server = new GraphQLServer({typeDefs, resolvers});
 
 server.start(() => {
     console.log("The server is up!")
-})
+});
+
+// sudo fuser -k port/tcp
+// server froce stop
