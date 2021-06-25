3dd1893c8fdc4c978e0f235b222c9eb624502a88
jaykabu <jaykabutarwala2@gmail.com>
2021-06-25 11:49:54 +0530

    create mutation and cratePost and createComment mutation with relational

--- src/Mutation.js
+++ src/Mutation.js
@@ -1,5 +1,5 @@
 import {GraphQLServer} from "graphql-yoga";
-import {v4 as uuidv4} from 'uuid'
+import {v4 as uuidv4} from 'uuid';
 
 
 //demo user data
@@ -88,6 +88,8 @@ const typeDefs = `
     
     type Mutation {
         createUser(name: String!, email: String, age: Int!): User!
+        createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+        createComment(text: String, author:ID!, post:ID!): Comment!
     }
     
     type User {
@@ -183,6 +185,53 @@ const resolvers = {
 			users.push(user);
 
 			return user;
+		},
+
+		createPost: (parent, args, ctx, info) => {
+
+			const userExists = users.some((user) => {
+				return user.id === args.author;
+			});
+
+			if (!userExists) {
+				throw Error('User not found')
+			}
+
+			const post = {
+				id: uuidv4(),
+				title: args.title,
+				body: args.body,
+				published: args.published,
+				author: args.author
+			};
+
+			posts.push((post))
+			return post;
+		},
+
+		createComment: (parent, args, ctx, info) => {
+			const userExists = users.some((user) => {
+				return user.id === args.author;
+			});
+
+			const postExists = posts.some(post => {
+				return post.id === args.post && post.published === true;
+			})
+
+			if (!userExists || !postExists) {
+				throw Error('Unable to find user and post')
+			}
+
+			const comment = {
+				id: uuidv4(),
+				text: args.text,
+				author: args.author,
+				post: args.post
+			}
+
+			comments.push(comment);
+
+			return comment;
 		}
 	},
 
